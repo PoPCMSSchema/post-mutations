60fbdb59f86f9ab554918f1d737888cabfb69dd4
Leonardo Losoviz <leo@getpop.org>
2020-11-06 23:18:27 +0800

    Moved getFormData to the Bridge

--- src/MutationResolvers/CreatePostMutationResolverTrait.php
+++ src/MutationResolvers/CreatePostMutationResolverTrait.php
@@ -6,9 +6,9 @@ namespace PoPSchema\PostMutations\MutationResolvers;
 
 trait CreatePostMutationResolverTrait
 {
-    public function execute(array &$errors, array &$errorcodes)
+    public function execute(array &$errors, array &$errorcodes, array $form_data)
     {
-        $post_id = $this->create($errors);
+        $post_id = $this->create($errors, $form_data);
         return $post_id;
     }
 }
--- src/MutationResolvers/UpdatePostMutationResolverTrait.php
+++ src/MutationResolvers/UpdatePostMutationResolverTrait.php
@@ -6,10 +6,10 @@ namespace PoPSchema\PostMutations\MutationResolvers;
 
 trait UpdatePostMutationResolverTrait
 {
-    public function execute(array &$errors, array &$errorcodes)
+    public function execute(array &$errors, array &$errorcodes, array $form_data)
     {
         $post_id = $_REQUEST[POP_INPUTNAME_POSTID];
-        $this->update($errors);
+        $this->update($errors, $form_data);
         return $post_id;
     }
 }
