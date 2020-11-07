c4f495c60c7c95056c491f27ae394903858efd16
Leonardo Losoviz <leo@getpop.org>
2020-11-07 13:40:19 +0800

    Migrated API for Posts

--- src/MutationResolvers/AbstractCreateUpdatePostMutationResolver.php
+++ src/MutationResolvers/AbstractCreateUpdatePostMutationResolver.php
@@ -4,9 +4,35 @@ declare(strict_types=1);
 
 namespace PoPSchema\PostMutations\MutationResolvers;
 
+use PoPSchema\Posts\Facades\PostTypeAPIFacade;
+use PoPSchema\PostMutations\Facades\PostTypeAPIFacade as MutationPostTypeAPIFacade;
 use PoPSchema\CustomPostMutations\MutationResolvers\AbstractCreateUpdateCustomPostMutationResolver;
 
 abstract class AbstractCreateUpdatePostMutationResolver extends AbstractCreateUpdateCustomPostMutationResolver
 {
+    protected function getCustomPostType()
+    {
+        $postTypeAPI = PostTypeAPIFacade::getInstance();
+        return $postTypeAPI->getPostCustomPostType();
+    }
 
+    /**
+     * @param array<string, mixed> $data
+     * @return mixed the ID of the updated post
+     */
+    protected function executeUpdateCustomPost(array $data)
+    {
+        $postTypeAPI = MutationPostTypeAPIFacade::getInstance();
+        return $postTypeAPI->updatePost($data);
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     * @return mixed the ID of the created post
+     */
+    protected function executeCreateCustomPost(array $data)
+    {
+        $postTypeAPI = MutationPostTypeAPIFacade::getInstance();
+        return $postTypeAPI->createPost($data);
+    }
 }
