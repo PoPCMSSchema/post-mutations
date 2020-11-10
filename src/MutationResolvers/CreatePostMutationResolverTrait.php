5812f079390424b290d1eabbd2203a4c37883dc6
Leonardo Losoviz <leo@getpop.org>
2020-11-10 16:09:03 +0800

    Added phpdoc to `execute`

--- src/MutationResolvers/CreatePostMutationResolverTrait.php
+++ src/MutationResolvers/CreatePostMutationResolverTrait.php
@@ -6,6 +6,9 @@ namespace PoPSchema\PostMutations\MutationResolvers;
 
 trait CreatePostMutationResolverTrait
 {
+    /**
+     * @return mixed
+     */
     public function execute(array $form_data)
     {
         return $this->create($form_data);
--- src/MutationResolvers/UpdatePostMutationResolverTrait.php
+++ src/MutationResolvers/UpdatePostMutationResolverTrait.php
@@ -6,6 +6,9 @@ namespace PoPSchema\PostMutations\MutationResolvers;
 
 trait UpdatePostMutationResolverTrait
 {
+    /**
+     * @return mixed
+     */
     public function execute(array $form_data)
     {
         return $this->update($form_data);
