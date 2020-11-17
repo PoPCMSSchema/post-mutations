fdbc7e0838418afb06a64519a316c16cb95dc9a9
Leonardo Losoviz <leo@getpop.org>
2020-11-17 23:46:59 +0800

    Import classes

--- src/MutationResolvers/CreatePostMutationResolver.php
+++ src/MutationResolvers/CreatePostMutationResolver.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace PoPSchema\PostMutations\MutationResolvers;
 
+use PoPSchema\CustomPostMutations\MutationResolvers\CreateCustomPostMutationResolverTrait;
+
 class CreatePostMutationResolver extends AbstractCreateUpdatePostMutationResolver
 {
     use CreateCustomPostMutationResolverTrait;
--- src/MutationResolvers/UpdatePostMutationResolver.php
+++ src/MutationResolvers/UpdatePostMutationResolver.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace PoPSchema\PostMutations\MutationResolvers;
 
+use PoPSchema\CustomPostMutations\MutationResolvers\UpdateCustomPostMutationResolverTrait;
+
 class UpdatePostMutationResolver extends AbstractCreateUpdatePostMutationResolver
 {
     use UpdateCustomPostMutationResolverTrait;
