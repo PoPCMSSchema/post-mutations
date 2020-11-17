c1028557ce76216966086ca00cd1d9f483822d72
Leonardo Losoviz <leo@getpop.org>
2020-11-17 16:00:52 +0800

    @TODO: Migrate when package "Categories" is completed

--- src/MutationResolvers/CreateUpdatePostMutationResolverTrait.php
+++ src/MutationResolvers/CreateUpdatePostMutationResolverTrait.php
@@ -6,8 +6,9 @@ namespace PoPSchema\PostMutations\MutationResolvers;
 
 trait CreateUpdatePostMutationResolverTrait
 {
-    protected function showCategories()
-    {
-        return !empty(\PoP_Application_Utils::getContentpostsectionCats());
-    }
+    // @TODO: Migrate when package "Categories" is completed
+    // protected function showCategories()
+    // {
+    //     return !empty(\PoP_Application_Utils::getContentpostsectionCats());
+    // }
 }
